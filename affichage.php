0a5af2831bed19a06f71e920a51c5522e1c3ad54
loelia-c <43027766+loelia-c@users.noreply.github.com>
2020-06-26 11:00:53 +0200

    Etape 4 + commentaires

--- affichage.php
+++ affichage.php
@@ -8,69 +8,155 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     <title>Loélia COUTEAU - Evaluation PHP</title>
 </head>
-<body>
-<div class="jumbotron jumbotron-fluid">
-    <div class="container">
-        <h1 class="display-4">Informations fournies</h1>
-        <p class="lead">Veuillez trouver ci-dessous les informations que vous nous avez fournies</p>
-    </div>
-</div>
-<div class="container">
-    <div class="row">
-        <div class="col-lg-2">
-            Nom :
+    <body>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">Informations fournies</h1>
+                <p class="lead">Veuillez trouver ci-dessous les informations que vous nous avez fournies</p>
+            </div>
         </div>
-        <div class="col-lg-10">
-            <?php echo $_GET['nom']; ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-2">
-            Ville :
-        </div>
-        <div class="col-lg-10">
-            <?php echo $_GET['ville']; ?>
-        </div>    </div>
-    <div class="row">
-        <div class="col-lg-2">
-            Téléphone :
-        </div>
-        <div class="col-lg-10">
-            <?php echo $_GET['phone']; ?>
-        </div>    </div>
-    <div class="row">
-        <div class="col-lg-2">
-            Mail :
-        </div>
-        <div class="col-lg-10">
-            <?php echo $_GET['mail']; ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-2">
-            Type de projet :
-        </div>
-        <div class="col-lg-10">
-            <?php echo $_GET['type-projet']; ?>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-2">
-            Options du projet :
-        </div>
-        <div class="col-lg-10">
-            <ul>
-                <?php
-                $tab_opt = $_GET['optionsProjet'];
-                for ($i=0; $i < count($tab_opt); $i++)
-                {
-                    echo '<li>'.$tab_opt[$i].'</li>';
-                }
-                ?>
-            </ul>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    Nom :
+                </div>
+                <div class="col-lg-10">
+                    <?php
+                    /*On récupère le nom et on le retourne*/
+                    echo $_GET['nom'];
+                    ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-2">
+                    Ville :
+                </div>
+                <div class="col-lg-10">
+                    <?php
+                    /*On récupère la ville*/
+                     $ville = $_GET['ville'];
+                    /*On défini l'agence par défaut à Paris puisqu'il s'agit du cas le plus général*/
+                     $agence ='Paris';
+                    /*Si la ville du prospect est Nantes, alors l'agence est Nantes*/
+                    if($ville == 'Nantes'){
+                        $agence = 'Nantes';
+                    }
+                    /*Sinon, si la ville du prospect est Marseille, alors l'agence est Marseille*/
+                    elseif ($ville == 'Marseille'){
+                        $agence = 'Marseille';
+                    }
+                    /*On retourne la ville*/
+                    echo $ville;
+                    echo '<p>Votre demande sera envoyé à l\'agence de '.$agence.' car vous êtes sur la ville de '.$ville.'.</p>';
+                    ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-2">
+                    Téléphone :
+                </div>
+                <div class="col-lg-10">
+                    <?php
+                    /*On récupère le téléphone et on le retourne*/
+                    echo $_GET['phone'];
+                    ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-2">
+                    Mail :
+                </div>
+                <div class="col-lg-10">
+                    <?php
+                    /*On récupère le mail et on le retourne*/
+                    echo $_GET['mail'];
+                    ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-2">
+                    Type de projet :
+                </div>
+                <div class="col-lg-10">
 
+                    <?php
+                    /*On récupère le type de projet et on le retourne*/
+                    echo $_GET['type-projet'];
+                    ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-2">
+                    Options du projet :
+                </div>
+                <div class="col-lg-10">
+                    <ul>
+                        <?php
+                        /*On récupère les options cochées dans un tableau*/
+                        $tab_opt = $_GET['optionsProjet'];
+                        /*On parcours ce tableau et on retourne chaque valeur sous forme de liste*/
+                        for ($i=0; $i < count($tab_opt); $i++)
+                        {
+                            echo '<li>'.$tab_opt[$i].'</li>';
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+            <?php
+            /*On défini un tableau de type de projet en associant la clé (nom du type de projet) à sa valeur (prix)*/
+            $type_proj_price = array(
+                'Wordpress' => 800,
+                'VueJS' => 500
+            );
+            /*On défini un tableau d'options de projet en associant la clé (nom de l'option de projet) à sa valeur (prix)*/
+            $opt_proj_price = array(
+                    'Formulaire de contact avancé' => 150,
+                    'Adaptations mobile spécifiques' => 225,
+                    'Design personnalisé' => 310
+            )
+            ?>
+            <div class="row">
+                <div class="col-lg-2">
+                    Prix estimé :
+                </div>
+                <div class="col-lg-10">
+                    <?php
+                    /*Initialisation du prix à 0€*/
+                    $price = 0;
+                    /*On parcours le tableau de type de projet en récupérant la clé et la valeur*/
+                    foreach ($type_proj_price as $key => $value)
+                    {
+                        /*Si la clé correspond au nom du type de projet sélectionné par le prospect, alors on ajoute au prix le prix du type sélectionné*/
+                        if($key == $_GET['type-projet'])
+                        {
+                            $price = $price + $value;
+                        }
+                    }
+                    /*On parcours le tableau des options de projet en récupérant la clé et la valeur*/
+                    foreach ($opt_proj_price as $key => $value)
+                    {
+                        /*On parcours également le tableau des options sélectionnées par le prospect*/
+                        foreach ($tab_opt as $option)
+                        {
+                            /*Pour chaque option, si celle-ci correspond à la clé, alors on ajoute le prix de l'option au prix total*/
+                            if($key == $option)
+                            {
+                                $price = $price + $value;
+                            }
+                        }
+
+                    }
+                    /*Si toutes les options sont cochées, alors on applique au prix total une réduction de 10%*/
+                    if (count($tab_opt) == 3){
+                        $price = $price*0.9;
+                    }
+                    /*On affiche le prox en €*/
+                    echo $price.'€';
+
+                    ?>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-</body>
+    </body>
 </html>
--- index.php
+++ index.php
@@ -15,50 +15,57 @@
         <p class="lead">Veuillez remplir le formulaire ci-dessous</p>
     </div>
 </div>
+<!-- Déclaration du formulaire, méthode GET, renvoi sur la page affichage.php une fois validé-->
 <form action="affichage.php" method="get">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
                 <div class="form-group">
+                    <!-- label et input nom, type texte, obligatoire-->
                     <label for="nom">Nom :</label>
                     <input type="text" class="form-control" id="nom" name="nom" required width="50">
                 </div>
                 <div class="form-group">
+                    <!-- label et input ville, type texte, obligatoire-->
                     <label for="ville">Ville</label>
                     <input type="text" class="form-control" id="ville" name="ville" required>
                 </div>
                 <div class="form-group">
+                    <!-- label et input téléphone, type nombre, obligatoire-->
                     <label for="phone">Téléphone</label>
-                    <input type="number" class="form-control" id="phone" name="phone" required minlength="10">
+                    <input type="number" class="form-control" id="phone" name="phone" required>
                 </div>
                 <div class="form-group">
+                    <!-- label et input mail, type email, obligatoire-->
                     <label for="mail">Mail</label>
                     <input type="email" class="form-control" id="mail" name="mail" required>
                 </div>
                 <div class="form-group">
                     <label for="type-projet">Type de projet</label>
+                    <!-- label et liste déroulante-->
                     <select class="form-control" name="type-projet" id="type-projet">
                         <option value="">>> Sélectionnez un type de projet <<</option>
-                        <option value="wordpress">Wordpress</option>
-                        <option value="vueJS">VueJS</option>
+                        <option value="Wordpress">Wordpress  |  800€</option>
+                        <option value="VueJS">VueJS  |  500€</option>
                     </select>
                 </div>
                 <div class="form-group">
                     <p>Options du projet</p>
+                    <!-- labels et cases à cocher-->
                     <div>
-                        <input type="checkbox" id="contact-avance" name="optionsProjet[]" value="Formulaire de contact avancé">
-                        <label for="contact-avance">Formulaire de contact avancé</label>
+                        <input type="checkbox" name="optionsProjet[]" value="Formulaire de contact avancé">
+                        <label>Formulaire de contact avancé  |  150€</label>
                     </div>
-                   <div>
-                       <input type="checkbox" id="adapt-mobile" name="optionsProjet[]" value="Adaptations mobile spécifiques">
-                       <label for="adapt-mobile">Adaptations mobile spécifiques</label>
-                   </div>
                     <div>
-                        <input type="checkbox" id="design-perso" name="optionsProjet[]" value="Design personnalisé">
-                        <label for="design-perso">Design personnalisé</label>
+                        <input type="checkbox" name="optionsProjet[]" value="Adaptations mobile spécifiques">
+                        <label>Adaptations mobile spécifiques  |  225€</label>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="optionsProjet[]" value="Design personnalisé">
+                        <label>Design personnalisé  |  310€</label>
                     </div>
                 </div>
-
+                <!-- Bouton de validation / envoi du formulaire et redirection vers affichage.php -->
                 <button type="submit" class="btn btn-outline-info">Valider</button>
             </div>
         </div>
